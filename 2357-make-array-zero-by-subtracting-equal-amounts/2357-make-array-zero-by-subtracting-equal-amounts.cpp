d9328ae24e11b0425daa70e318a58557352a5794
MagdyGeorge1 <112896733+MagdyGeorge1@users.noreply.github.com>
2023-01-14 00:02:36 +0200

    Time: 6 ms (34.26%), Space: 8.3 MB (61.61%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -3,10 +3,6 @@ public:
     int minimumOperations(vector<int>& nums) {
         sort(nums.begin() , nums.end());
         int sum = 0 , prv = 0 , op = 0;
-        // int l = upper_bound(nums.begin() , nums.end() , 0) - nums.begin();
-        // if (l == nums.size())
-        //     return 0;
-        // cout << l ;
         for(int i = 0 ; i < nums.size() ; i++)
         {
            nums[i] -= min (nums[i] , prv);
