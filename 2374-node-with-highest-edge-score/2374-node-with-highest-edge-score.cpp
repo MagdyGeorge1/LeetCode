b5b42a0e5fb3d4d533df78a26df378ed849fea21
MagdyGeorge1 <112896733+MagdyGeorge1@users.noreply.github.com>
2023-01-15 03:40:05 +0200

    Time: 248 ms (68.04%), Space: 293.8 MB (5.05%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -3,10 +3,19 @@ public:
     int edgeScore(vector<int>& edges) {
          int n = edges.size();
         vector<long long> degree(2e5 +10 , 0);
-        
+        long long mn = 1e18 , mx = 0;
         for(int i = 0 ; i< n; i++)
-            degree[edges[i]]+=i;
+        {
+        degree[edges[i]]+=i;
+        if(degree[edges[i]] > mx )
+            mx = degree[edges[i]] , mn = edges[i];
+            else if ( degree[edges[i]] == mx )
+            {
+                mn = min(1LL* edges[i] , mn);
+            } 
+            
+        }
         
-        return max_element(degree.begin() , degree.end()) - degree.begin() ;
+        return mn ;
     }
 };
