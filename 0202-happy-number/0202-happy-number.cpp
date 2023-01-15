ec751c20b20edb875fbdb51625b33c836859ac46
MagdyGeorge1 <112896733+MagdyGeorge1@users.noreply.github.com>
2023-01-15 03:26:10 +0200

    Time: 6 ms (26.81%), Space: 6.4 MB (26.36%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_map<int,bool> mp;
+        unordered_set<int> mp;
         while (n > 1)
-        {
+        {            
             int tmp = n , sum = 0;
             while (tmp)
             {
-                sum += (tmp%10 ) * (tmp%10);
+                sum += (tmp%10) * (tmp%10);
                 tmp/=10;
             }
            n = sum;
             if (mp.count(sum))
-                return false;
-            mp[sum] = 1; 
-        
+                return  false;
+            mp.insert(sum);
         }
-        
         return true;
     }
 };
