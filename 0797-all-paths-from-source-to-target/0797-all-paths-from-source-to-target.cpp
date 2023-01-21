d55bfc73f600046caba37c81e02d40fc8810d971
MagdyGeorge1 <112896733+MagdyGeorge1@users.noreply.github.com>
2023-01-21 04:00:39 +0200

    Time: 10 ms (89.42%), Space: 11.9 MB (60.08%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     vector<int>path;
     vector<vector<int>>ans;
-    int vis[100];
     void dfs(int node , vector<vector<int>>& graph)
     {
-        vis[node] = 1;
         path.push_back(node);
-        for(int ch : graph[node])
-        {
-            if(!vis[ch])
+        for(int ch : graph[node])    
                 dfs(ch , graph);
-        }
+        
         if(node == graph.size() -1 )
             ans.push_back(path);
-        vis[node] = 0;
         path.pop_back();
                     
     }
