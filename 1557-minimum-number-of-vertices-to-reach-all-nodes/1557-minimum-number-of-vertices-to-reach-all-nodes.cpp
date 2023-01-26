374e8dc6658f5c2ed4c9b93dfa163e36fd7eaf24
MagdyGeorge1 <112896733+MagdyGeorge1@users.noreply.github.com>
2023-01-26 04:15:53 +0200

    Time: 368 ms (55.66%), Space: 106.8 MB (29.01%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         vector<int>indegree(n,0);
-        for(auto iter : edges)
-        {
+        for(auto iter : edges)        
             indegree[iter[1]]++;
-        }
+        
         vector<int>ans;
-        for(int i = 0 ; i<n;i++)
-        {
+        for(int i = 0 ; i<n;i++)        
             if(!indegree[i])
                 ans.push_back(i);
-        }
+        
         return ans;
     }
 };
