8a52cd7ca6990ac43c901bd6728dc61ae25e2d57
MagdyGeorge1 <112896733+MagdyGeorge1@users.noreply.github.com>
2023-03-28 01:41:13 +0200

    Time: 63 ms (64.43%), Space: 22 MB (67.91%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.cpp
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -4,16 +4,16 @@ public:
     vector<string> simplifiedFractions(int n) 
     {
         vector < string > ret;
-       for (int i = 1 ; i < n ; i++)
+       for (int i = 2 ; i <= n ; i++)
        {
 
-           for (int j = 2 ; j <= n ; j++)
+           for (int j = 1 ; j < i ; j++)
            {
-               if (i >= j)
-                   continue;
+               // if (i >= j)
+               //     continue;
                if (__gcd(i , j) == 1)
                {
-                   string tmp = to_string(i)+ "/" + to_string(j);
+                   string tmp = to_string(j)+ "/" + to_string(i);
                    ret.push_back(tmp);
                }
            }
